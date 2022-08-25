633cefeb2e20b253bc419b6c320429b2594111f0
sujaul karim <soejaoel@gmail.com>
2022-08-25 10:09:39 +0700

    add ne intent method

--- CameraX/src/main/java/com/chareem/camerax/cameraxbasic/ChareemCameraX.kt
+++ CameraX/src/main/java/com/chareem/camerax/cameraxbasic/ChareemCameraX.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
 import com.chareem.camerax.cameraxbasic.utils.CameraHelper
@@ -29,6 +30,7 @@ class ChareemCameraX{
     private var dirName = ""
     private var resultCode = CameraHelper.RESULT_CODE
     private var mCcontext: Activity? = null
+    private var mResultLauncher: ActivityResultLauncher<Intent>? = null
     private lateinit var mInstance: ChareemCameraX
     companion object {
         fun with(context: Activity) : ChareemCameraX{
@@ -80,6 +82,11 @@ class ChareemCameraX{
         return mInstance
     }
 
+    fun setResultLauncher(mResultLaunchers: ActivityResultLauncher<Intent>): ChareemCameraX {
+        mResultLauncher = mResultLaunchers
+        return mInstance
+    }
+
     fun launchCamera() {
         mCcontext?.let { context ->
             if (CameraHelper.hasCamera(context)) {
@@ -118,7 +125,7 @@ class ChareemCameraX{
                                 cameraIntent.putExtra(CameraHelper.Arguments.FACE_DETECTION, isUseFaceDetection)
                                 cameraIntent.putExtra(CameraHelper.Arguments.IMAGE_NAME, fileName)
                                 cameraIntent.putExtra(CameraHelper.Arguments.DIR_NAME, dirName)
-                                context.startActivityForResult(cameraIntent, resultCode)
+                                mResultLauncher?.launch(cameraIntent) ?: context.startActivityForResult(cameraIntent, resultCode)
                             } else {
                                 Log.d("akdkodko", "finish")
                                 Toast.makeText(
@@ -139,44 +146,44 @@ class ChareemCameraX{
     }
 
     fun getCameraIntent(): Intent? {
-        if (mCcontext == null) return null
-        val context = mCcontext!!
-        if (CameraHelper.hasCamera(context)) {
-            val permissions = java.util.ArrayList<String>()
-            permissions.add(Manifest.permission.CAMERA)
-            permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-            if (isUseTimestamp) {
-                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
-                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
-            }
-            for (permision in permissions) {
-                if (ActivityCompat.checkSelfPermission(
-                        context,
-                        permision
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    Toast.makeText(
-                        context,
-                        "You need to granted permission $permision first",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return null
+        mCcontext?.let { context ->
+            if (CameraHelper.hasCamera(context)) {
+                val permissions = java.util.ArrayList<String>()
+                permissions.add(Manifest.permission.CAMERA)
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                if (isUseTimestamp) {
+                    permissions.add(Manifest.permission.ACCESS_FINE_LOCATION)
+                    permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION)
                 }
+                for (permision in permissions) {
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            permision
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        Toast.makeText(
+                            context,
+                            "You need to granted permission $permision first",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return null
+                    }
+                }
+                val cameraIntent = Intent(context, CameraXActivity::class.java)
+                cameraIntent.putExtra(CameraHelper.Arguments.SHOW_LOCATION, isUseTimestamp)
+                cameraIntent.putExtra(CameraHelper.Arguments.MOCK_LOCATION, isUseMockDetection)
+                cameraIntent.putExtra(CameraHelper.Arguments.DEVAULT_LAT, latitude)
+                cameraIntent.putExtra(CameraHelper.Arguments.DEVAULT_LON, longitude)
+                cameraIntent.putExtra(CameraHelper.Arguments.CAMERA_FACING_TYPE, cameraType)
+                cameraIntent.putExtra(CameraHelper.Arguments.FACE_DETECTION, isUseFaceDetection)
+                cameraIntent.putExtra(CameraHelper.Arguments.IMAGE_NAME, fileName)
+                cameraIntent.putExtra(CameraHelper.Arguments.DIR_NAME, dirName)
+                return cameraIntent
             }
-            val cameraIntent = Intent(context, CameraXActivity::class.java)
-            cameraIntent.putExtra(CameraHelper.Arguments.SHOW_LOCATION, isUseTimestamp)
-            cameraIntent.putExtra(CameraHelper.Arguments.MOCK_LOCATION, isUseMockDetection)
-            cameraIntent.putExtra(CameraHelper.Arguments.DEVAULT_LAT, latitude)
-            cameraIntent.putExtra(CameraHelper.Arguments.DEVAULT_LON, longitude)
-            cameraIntent.putExtra(CameraHelper.Arguments.CAMERA_FACING_TYPE, cameraType)
-            cameraIntent.putExtra(CameraHelper.Arguments.FACE_DETECTION, isUseFaceDetection)
-            cameraIntent.putExtra(CameraHelper.Arguments.IMAGE_NAME, fileName)
-            cameraIntent.putExtra(CameraHelper.Arguments.DIR_NAME, dirName)
-            return cameraIntent
-        }
-        Toast.makeText(context, "Error camera occurred, you have no camera", Toast.LENGTH_SHORT)
-            .show()
-        return null
+            Toast.makeText(context, "Error camera occurred, you have no camera", Toast.LENGTH_SHORT)
+                .show()
+            return null
+        } ?: return null
     }
 }
